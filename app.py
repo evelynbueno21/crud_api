951b515ace21878de11a0a23f19948a283874b25
evelynbueno21 <167826118+evelynbueno21@users.noreply.github.com>
2025-08-03 20:17:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost:5432/crud_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mypassword@localhost:5432/crud_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
